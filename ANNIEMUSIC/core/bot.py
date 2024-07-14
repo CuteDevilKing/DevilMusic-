0abda41c7c2fd3de54b7e351f8a93e19c8e06248
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-07-14 15:27:59 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -10,7 +10,7 @@ class JARVIS(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Annie is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="❍⏤͟͞●𝐍❍ʈʈ𝐘༎꯭ࠫ𓆩 ⃝⃪⃕𝐅𝙰ɳ𝗕๏‌𝕪❜𝆺꯭𝅥𝆺꯭𝅥༎ࠫ,",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
