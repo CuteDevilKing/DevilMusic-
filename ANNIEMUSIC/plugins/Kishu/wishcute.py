44283b693f0a84221a1a930b29a8b3a7eaec9219
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-07-14 16:43:12 +0530

    Update wishcute.py

--- ANNIEMUSIC/plugins/Kishu/wishcute.py
+++ ANNIEMUSIC/plugins/Kishu/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from ANNIEMUSIC import app 
 
-SUPPORT_CHAT = "BWANDARLOK"
+SUPPORT_CHAT = "https://t.me/+crSKsZlbdRk3OTg1"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
