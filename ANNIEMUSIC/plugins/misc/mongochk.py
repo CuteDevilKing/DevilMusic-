4cb40a7726f503fa76ce13113c589c2cb742b895
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-08-07 17:25:32 +0530

    Update mongochk.py

--- ANNIEMUSIC/plugins/misc/mongochk.py
+++ ANNIEMUSIC/plugins/misc/mongochk.py
@@ -2,13 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from ANNIEMUSIC import app as JARVIS
+from ANNIEMUSIC import app as DEVIL
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
 
 
-@JARVIS.on_message(filters.command("mongochk"))
+@DEVIL.on_message(filters.command("mongochk"))
 async def mongo_command(client, message: Message):
     if len(message.command) < 2:
         await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
