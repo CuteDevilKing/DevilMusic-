4c904da529673a412d89827cc209fa9a02a59ce2
𓆩𝄟≛⃝ 𝐜𝐮𝐭𝐞𓆩Devil𓆪🦋≛⃝king𝄟𓆪 <166118712+CuteDevilKing@users.noreply.github.com>
2024-08-07 17:34:32 +0530

    Update play.py

--- ANNIEMUSIC/plugins/play/play.py
+++ ANNIEMUSIC/plugins/play/play.py
@@ -313,7 +313,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await JARVIS.stream_call(url)
+                await DEVIL.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
